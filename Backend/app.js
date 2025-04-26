e744baa7975db41b74c7c032ddbd42ad1096b0d0
ArthurJuma98 <arthurjuma98@gmail.com>
2025-04-26 11:55:22 +0300

    chnages in app configuration

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const clientRoutes = require('./routes/clientRoutes');
+const programRoutes = require('./routes/programRoutes');
+
 const app = express();
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use('/api/clients', clientRoutes);
+app.use('/api/programs', programRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Health Information System API');
+});
 
 module.exports = app;
